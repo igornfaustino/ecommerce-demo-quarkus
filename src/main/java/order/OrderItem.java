be90f3aab20baa5346a1211345c6e549272237d3
Igor Neves Faustino <igor@nfaustino.com>
2022-12-14 07:48:36 -0300

    feat: add get and set

--- src/main/java/order/OrderItem.java
+++ src/main/java/order/OrderItem.java
@@ -1,6 +1,5 @@
 package order;
 
-import java.util.Date;
 import java.util.UUID;
 
 public class OrderItem {
@@ -11,6 +10,26 @@ public class OrderItem {
     public OrderItem(UUID idItem, int quantity, float price) {
         this.idItem = idItem;
         this.quantity = quantity;
+        this.price = price;
+    }
+
+    public UUID getIdItem() {
+        return this.idItem;
+    }
+
+    public int getQuantity() {
+        return this.quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public float getPrice() {
+        return this.price;
+    }
+
+    public void setPrice(float price) {
         this.price = price;
     }
 
